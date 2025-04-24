c144c73a2911fdf3dada18df9338f2f1e49ff72f
Leisure <107242024000080@stu.xisu.edu.cn>
2025-04-24 21:23:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oaMrT7d-jT3mibkZ7yL7aHMLw8Cc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'n-aJgCc8MngA0zUcpC071DkYBGdGY91sOeObK3lSrJI',
+      useTemplateId: 'q3CvxSd0yF0yh1dnfc10YgLG7mIeGntW9IQWSu5qLc4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-16',
       festivals: [
